9b3592f87462d9af28e2caafc720d89b88c36172
Yazdan Vahidi <yazdanvahidi@yahoo.com>
2024-10-14 14:06:59 +0330

    feat: add stats and suggestions

--- src/pages/StatsPage.tsx
+++ src/pages/StatsPage.tsx
@@ -1,7 +1,31 @@
 import { FC } from "react";
+import LotilAnimation from "../assets/lotties/building.json";
+import Lottie from "react-lottie";
+
+const defaultOptions = {
+  loop: true,
+  autoplay: true,
+  animationData: LotilAnimation,
+  rendererSettings: {
+    preserveAspectRatio: "xMidYMid slice",
+  },
+};
 
 const StatsPage: FC = () => {
-  return <div>StatsPage</div>;
+  return (
+    <section
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <h1>تحلیل ها و آمار بازدید</h1>
+      <Lottie options={defaultOptions} height={400} width={400} />
+      <h3>تیم فرز درحال آماده سازی این صحفه میباشد.</h3>
+    </section>
+  );
 };
 
 export default StatsPage;
--- src/pages/SuggestionsPage.tsx
+++ src/pages/SuggestionsPage.tsx
@@ -1,7 +1,19 @@
 import { FC } from "react";
 
 const SuggestionsPage: FC = () => {
-  return <div>SuggestionsPage</div>;
+  return (
+    <section
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <h1>پیشنهادات فرز</h1>
+      <h3>درحال حاضر پیشنهادی برای شما وجود ندارد...</h3>
+    </section>
+  );
 };
 
 export default SuggestionsPage;
