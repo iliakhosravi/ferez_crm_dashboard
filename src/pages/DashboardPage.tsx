2d6bb99a9cbdf6052088551949b31cc71fc44a92
elmishow group <elmishow.group@gmail.com>
2024-10-31 10:51:44 +0330

    fix: temporary add contact us

--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -1,6 +1,13 @@
-import { FC } from "react";
+import { FC, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 const DashboardPage: FC = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    navigate("/brand");
+  }, [navigate]);
+
   return <section></section>;
 };
 
--- src/pages/SpecialProductsPage.tsx
+++ src/pages/SpecialProductsPage.tsx
@@ -2,24 +2,36 @@ import { CardGiftcardOutlined } from "@mui/icons-material";
 import { FC } from "react";
 
 const SpecialProductsPage: FC = () => {
-  return <section>
-    <div style={{
-        width:"100%",
-        height: "10vh",
-        display:"flex",
-        justifyContent:"start",
-        alignItems:"center",
-        fontWeight:"bold",
-        color:"#1677FF",
-        borderBottom: "solid 1px #d7ebfa"
-      }}>
-        <div style={{display:"flex", gap:"8px"}}>
-        <CardGiftcardOutlined/>
-        محصولات نمونه
-
+  return (
+    <section>
+      <div
+        style={{
+          width: "100%",
+          height: "10vh",
+          display: "flex",
+          justifyContent: "start",
+          alignItems: "center",
+          borderBottom: "solid 1px #d7ebfa",
+        }}
+      >
+        <div style={{ display: "flex", alignItems: "center", gap: "8px" }}>
+          <CardGiftcardOutlined />
+          <h3 style={{ fontWeight: "bold", color: "#1677FF" }}>
+            محصولات نمونه
+          </h3>
         </div>
-        </div>
-  </section>;
+      </div>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <p>محصولی برای نمایش وجود ندارد</p>
+      </div>
+    </section>
+  );
 };
 
 export default SpecialProductsPage;
